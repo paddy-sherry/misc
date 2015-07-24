ab83214eeb8b7fc73a9920ae1ec960613d9b2654
paddy <hello@paddysherry.com>
2015-07-24 03:11:02 +0100

    Added tests and created index.html to demo it

---
+++ intercom/5-topN/src/requestHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+//A little file to handle the post request from the page and instantiate the class
+require_once 'topN.php';
+
+if(isset( $_POST['function'] ) && isset($_POST['n'])) {
+	//the number of integers we want to get back.
+	$n = filter_var($_POST['n'], FILTER_SANITIZE_NUMBER_INT);
+
+    if ($_POST['function'] == 'readfileInChunks') {
+    	$TopN = new TopN();
+		$TopN->readfileInChunks(TRUE, $n);
+    }   
+}
